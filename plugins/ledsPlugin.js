883789d7f71eead86121e1b9ddad28a72e898e93
rockagon <ragon@andrew.cmu.edu>
2023-09-27 18:15:05 +0200

    CHANGE

--- plugins/ledsPlugin.js
+++ plugins/ledsPlugin.js
@@ -1,40 +1,26 @@
 var onoff = require ( 'onoff');
-var resources = require('../resources/resources.json');
+var resources = require('./../resources/resources.json');
 
 var Gpio = onoff.Gpio, 
-	led1 = new Gpio(20, 'out'),
+	led1 = new Gpio(20, 'out')
 	led2 = new Gpio(21, 'out')
-var interval1 = null;
 
-function warming() {
+function warning() {
 	if (led1.readSync() === 0) { 
         resources.pi.actuators.leds.led1 = true 
         resources.pi.actuators.leds.led2 = false
 		led1.writeSync(1);
 		led2.writeSync(0);
-		console.log ( 'Alert Intruision');
+		console.log ('Alert Intruision, LED1 on');
 	}
     else {
         resources.pi.actuators.leds.led2 = true
         resources.pi.actuators.leds.led1 = false
 		led1.writeSync(0);
 		led2.writeSync(1);
-		console.log ( 'Alert Intruision');
+		console.log ('Alert Intruision LED 2 on');
 	}
 }
-function alert_Off() { 
-    resources.pi.actuators.leds.led2 = false
-    resources.pi.actuators.leds.led1 = false
-	clearInterval(interval1);
-	led1.writeSync(0); 
-	led1.unexport(); 
-	led2.writeSync(0); 
-	led2.unexport();
-	console.log('End warming')
-	process.exit(); 
-}
-process.on('SIGINT', alert_Off); //To turn the alert off by pressing Ctrl+C
-
-exports.module = {
-    warming, alert_Off
-}
+//process.on('SIGINT', alert_Off); //To turn the alert off by pressing Ctrl+C
+ 
+module.exports = { warning }
--- plugins/pirPlugin.js
+++ plugins/pirPlugin.js
@@ -1,18 +1,31 @@
 var onoff = require ( 'onoff');
-var ledsPlugin = require("ledsPlugin.js");
+var ledsPlugin = require("./ledsPlugin");
+var resources = require('./../resources/resources.json');
+
 var Gpio = onoff.Gpio, 
     pir = new Gpio(17, 'in', 'both');
 var interval1 = null;
-
 
 
 pir.watch(function (err, value) {
 	if (err) { exit(err);}  //In case of error of PIR
 	else if (value) {
 	   if (interval1) { clearInterval(interval1);} //To clear interval1 each time to keep blinking interval same
-	   interval1 = setInterval(ledsPlugin.warming(), 1000);	
+	   interval1 = setInterval(ledsPlugin.warning , 1000)
+	   console.log("PIR value is: " , value)
 	}
-	setTimeout(alert_Off , 30000) // To turn the alert off after 30s
-   }
+	setTimeout(alert_Off, 30000)
+	}
    )
-process.on('SIGINT', ledsPlugin.alert_Off()); //To turn the alert off by pressing Ctrl+C
+   function alert_Off() { 
+	clearInterval(interval1)
+	led1.writeSync(0)
+	led2.writeSync(0) 
+	led1.unexport()
+	led2.unexport()
+	console.log('No warning')
+	resources.pi.actuators.leds.led2 = false
+    resources.pi.actuators.leds.led1 = false
+	//process.exit(); 
+   }
+   
