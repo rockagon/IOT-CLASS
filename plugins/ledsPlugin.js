80225464e1643be0f19ee11cf306bcfbaad407eb
rockagon <ragon@andrew.cmu.edu>
2024-07-25 13:41:18 +0200

    kfkr

--- plugins/ledsPlugin.js
+++ plugins/ledsPlugin.js
@@ -2,22 +2,19 @@ var onoff = require ( 'onoff');
 var resources = require('./../resources/resources.json');
 
 var Gpio = onoff.Gpio, 
-	led1 = new Gpio(20, 'out'),
-	led2 = new Gpio(21, 'out')
+	led1 = new Gpio(20, 'out')
 
 function warning() {
 	if (led1.readSync() === 0) { 
         resources.pi.actuators.leds.led1 = true 
         resources.pi.actuators.leds.led2 = false
 		led1.writeSync(1);
-		led2.writeSync(0);
 		console.log ( 'Alert Intrusion, LED1 on');
 	}
     else {
         resources.pi.actuators.leds.led2 = true
         resources.pi.actuators.leds.led1 = false
 		led1.writeSync(0);
-		led2.writeSync(1);
 		console.log ( 'Alert Intrusion, LED 2 on');
 	}
 }
