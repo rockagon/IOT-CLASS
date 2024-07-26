52ca33c52fa732015fa8688c4ecd036b06323832
rockagon <ragon@andrew.cmu.edu>
2024-07-26 18:25:26 +0200

    oooh

--- plugins/pirPlugin.js
+++ plugins/pirPlugin.js
@@ -1,48 +1,34 @@
 var Gpio = require('onoff').Gpio;
-// var Gpio = onoff
 
 var led1 = new Gpio(532, 'out')
 var pir = new Gpio(529, 'in', 'both');
 //var resources = require('./../resources/resources.json');
 var interval1 = null;
 
-function warning() {
-	if (led1.readSync() === 0) { 
-       // resources.pi.actuators.leds.led1 = true 
-	   console.log ( 'Alert Intrusion');	
-	   led1.writeSync(1);
-	}
-    else {
-       // resources.pi.actuators.leds.led2 = true
-	   console.log ( 'Alert Intrusion');	
-	   led1.writeSync(0);	
-	}
-}
-
 function alert_Off() { 
-  //  resources.pi.actuators.leds.led1 = false
-	clearInterval(interval1);
-	led1.writeSync(0); 
-	led1.unexport(); 
-	console.log('End warning')
-	process.exit(); 
-}
+	//  resources.pi.actuators.leds.led1 = false
+	  clearInterval(interval1);
+	  led1.writeSync(0); 
+	  led1.unexport(); 
+	  console.log('End warning')
+	  process.exit(); 
+    }
 
 pir.watch(function (err, value) {
 	if (err) { 
 		console.log('Error');
-		exit(err);}  //In case of error of PIR
+		exit(err);
+	}  //In case of error of PIR
 	else if (value) {
 		console.log("PIR value is: " , value)
 	   if (interval1) {clearInterval(interval1);} //To clear interval1 each time to keep blinking interval same
 	   setInterval(() => {
-		warning()
-	}, 1000);  
-	   console.log("PIR value is: " , value)
-	   //resources.pi.sensors.pir.value = true
+		led1.writeSync(1);
+	}, 2000);  
 	}
-	setTimeout(alert_Off , 30000) // To turn the alert off after 30s
-   }
+	}
    )
+
+   alert_Off()
 
 //console.log("Rock")
