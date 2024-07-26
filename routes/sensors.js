11f8fcd86c43eb1244e087b6afde5bd5537f9abc
rockagon <141149208+rockagon@users.noreply.github.com>
2024-07-26 23:10:13 +0200

    Update sensors.js
    update

--- routes/sensors.js
+++ routes/sensors.js
@@ -7,22 +7,74 @@ router.route('/').get(function (req, res, next) {
     res.send(resources.pi.sensors);
 });
 
-router.route('/pirs').get(function (req, res, next) {
-    res.send(resources.pi.sensors.pirs);
+router.route('/pir').get(function (req, res, next) {
+    res.send(resources.pi.sensors.pir);
 });
 
-router.route('/pirs/pir1').get(function (req, res, next) {
-    res.send(resources.pi.sensors.pirs.pir1);
+router.route('/pir/value').get(function (req, res, next) {
+res.send(resources.pi.sensors.pir.value);
+});
+router.route('/pir/name').get(function (req, res, next) {
+    res.send(resources.pi.sensors.pir.name);
+});
+router.route('/pir/description').get(function (req, res, next) {
+    res.send(resources.pi.sensors.pir.description);
 });
 
-router.route('/pirs/pir1/value').get(function (req, res, next) {
-res.send(resources.pi.sensors.pirs.pir1.value);
+
+
+router.route('/ldr').get(function (req, res, next) {
+    res.send(resources.pi.sensors.ldr);
 });
-router.route('/pirs/pir1/name').get(function (req, res, next) {
-    res.send(resources.pi.sensors.pirs.pir1.name);
+
+router.route('/ldr/value').get(function (req, res, next) {
+res.send(resources.pi.sensors.ldr.value);
 });
-router.route('/pirs/pir1/description').get(function (req, res, next) {
-    res.send(resources.pi.sensors.pirs.pir1.description);
+router.route('/ldr/name').get(function (req, res, next) {
+    res.send(resources.pi.sensors.ldr.name);
 });
+router.route('/ldr/description').get(function (req, res, next) {
+    res.send(resources.pi.sensors.ldr.description);
+});
+
+
+
+router.route('/dht22').get(function (req, res, next) {
+    res.send(resources.pi.sensors.dht22);
+});
+
+router.route('/dht22/temperature').get(function (req, res, next) {
+res.send(resources.pi.sensors.dht22.temperature);
+});
+
+router.route('/dht22/temperature/name').get(function (req, res, next) {
+res.send(resources.pi.sensors.dht22.temperature.name);
+});
+
+router.route('/dht22/temperature/description').get(function (req, res, next) {
+res.send(resources.pi.sensors.dht22.temperature.description);
+});
+
+router.route('/dht22/temperature/value').get(function (req, res, next) {
+res.send(resources.pi.sensors.dht22.temperature.value);
+});
+
+
+
+router.route('/dht22/humidity').get(function (req, res, next) {
+res.send(resources.pi.sensors.dht22.humidity);
+});
+
+router.route('/dht22/temperature/name').get(function (req, res, next) {
+res.send(resources.pi.sensors.dht22.humidity.name);
+});
+
+router.route('/dht22/temperature/description').get(function (req, res, next) {
+res.send(resources.pi.sensors.dht22.humidity.description);
+});
+router.route('/dht22/temperature/value').get(function (req, res, next) {
+res.send(resources.pi.sensors.dht22.humidity.value);
+});
+
 
 module.exports = router;
