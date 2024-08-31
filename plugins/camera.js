fdc6f7850a2c586f125314c69552f7396ca6c32c
rockagon <141149208+rockagon@users.noreply.github.com>
2024-08-31 23:49:41 +0200

    Update camera.js

--- plugins/camera.js
+++ plugins/camera.js
@@ -1,28 +1,127 @@
 // plugins/camera.js
 const { spawn } = require('child_process');
+const EventEmitter = require('events');
 
-const startCameraStream = (res) => {
-  // Use libcamera to start streaming
-  const libcameraProcess = spawn('libcamera-vid', ['--inline', '-t', '0', '-o', '-', '--width', '640', '--height', '480']);
+class CameraStream extends EventEmitter {
+  constructor() {
+    super();
+    this.clients = [];
+    this.startStream();
+  }
 
-  // Set the response headers to indicate a video stream
-  res.setHeader('Content-Type', 'video/mjpeg');
+  startStream() {
+    // Spawn the libcamera-vid process
+    this.libcameraProcess = spawn('libcamera-vid', [
+      '--inline',
+      '-t',
+      '0',
+      '--codec',
+      'h264',
+      '--bitrate',
+      '2000000',
+      '-o',
+      '-', // Output to stdout
+      '--width',
+      '1280',
+      '--height',
+      '720',
+      '--framerate',
+      '25',
+      '--profile',
+      'baseline',
+    ]);
 
-  // Pipe the stdout of the libcamera process to the response
-  libcameraProcess.stdout.pipe(res);
+    // Spawn ffmpeg to convert H264 to HLS
+    this.ffmpegProcess = spawn('ffmpeg', [
+      '-i',
+      'pipe:0',
+      '-c:v',
+      'copy',
+      '-f',
+      'hls',
+      '-hls_time',
+      '1',
+      '-hls_list_size',
+      '5',
+      '-hls_flags',
+      'delete_segments',
+      '/dev/stdout',
+    ]);
 
-  // Handle process errors
-  libcameraProcess.on('error', (err) => {
-    console.error('Error starting camera stream:', err);
-    res.status(500).send('Error starting camera stream');
-  });
+    // Pipe libcamera-vid output to ffmpeg input
+    this.libcameraProcess.stdout.pipe(this.ffmpegProcess.stdin);
 
-  // Handle process exit
-  libcameraProcess.on('exit', (code) => {
-    if (code !== 0) {
-      console.error(`libcamera-vid process exited with code ${code}`);
+    // Handle ffmpeg output
+    this.ffmpegProcess.stdout.on('data', (data) => {
+      this.clients.forEach((res) => {
+        res.write(data);
+      });
+    });
+
+    this.ffmpegProcess.stderr.on('data', (data) => {
+      console.error(`ffmpeg stderr: ${data}`);
+    });
+
+    this.libcameraProcess.stderr.on('data', (data) => {
+      console.error(`libcamera-vid stderr: ${data}`);
+    });
+
+    this.libcameraProcess.on('error', (err) => {
+      console.error('Error starting libcamera-vid:', err);
+      this.emit('error', err);
+    });
+
+    this.ffmpegProcess.on('error', (err) => {
+      console.error('Error starting ffmpeg:', err);
+      this.emit('error', err);
+    });
+
+    this.libcameraProcess.on('exit', (code, signal) => {
+      console.error(`libcamera-vid exited with code ${code} and signal ${signal}`);
+      this.emit('exit', { code, signal });
+    });
+
+    this.ffmpegProcess.on('exit', (code, signal) => {
+      console.error(`ffmpeg exited with code ${code} and signal ${signal}`);
+      this.emit('exit', { code, signal });
+    });
+  }
+
+  addClient(res) {
+    // Set appropriate headers for HLS stream
+    res.writeHead(200, {
+      'Content-Type': 'application/vnd.apple.mpegurl',
+      'Connection': 'keep-alive',
+      'Cache-Control': 'no-cache',
+    });
+
+    this.clients.push(res);
+    console.log(`Client connected. Total clients: ${this.clients.length}`);
+
+    // Remove client on close
+    res.on('close', () => {
+      this.clients = this.clients.filter((client) => client !== res);
+      console.log(`Client disconnected. Total clients: ${this.clients.length}`);
+      if (this.clients.length === 0) {
+        // No clients left, stop the stream
+        this.stopStream();
+      }
+    });
+  }
+
+  stopStream() {
+    if (this.libcameraProcess) {
+      this.libcameraProcess.kill();
+      this.libcameraProcess = null;
     }
-  });
-};
+    if (this.ffmpegProcess) {
+      this.ffmpegProcess.kill();
+      this.ffmpegProcess = null;
+    }
+    console.log('Camera and ffmpeg processes killed due to no clients.');
+  }
+}
 
-module.exports = { startCameraStream };
+const cameraStream = new CameraStream();
+
+module.exports = cameraStream;
